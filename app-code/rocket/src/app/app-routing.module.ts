b4aee67085d94f0df714d4e407d96c46f28fd3bf
Carlos Adolfo Ortiz Quiros (Mac) <carlosaortiz@yahoo.com>
2018-05-27 10:31:22 -0500

    Adds routing

--- app-code/rocket/src/app/app-routing.module.ts
+++ app-code/rocket/src/app/app-routing.module.ts
@@ -1,34 +1,25 @@
 /*----------------------------------------------------------------------------*/
-/* Source File:   APP.MODULE.TS                                               */
-/* Description:   Used to bootstrap an Angular Application                    */
+/* Source File:   APP-ROUTING.MODULE.TS                                       */
+/* Description:   Used to define application routing (page navigation)        */
 /* Author:        Carlos Adolfo Ortiz Quirós (COQ)                            */
-/* Date:          May.24/2018                                                 */
+/* Date:          May.27/2018                                                 */
 /* Last Modified: May.27/2018                                                 */
 /* Version:       1.1                                                         */
 /* Copyright (c), 2018 CSoftZ                                                 */
 /*----------------------------------------------------------------------------*/
 /*-----------------------------------------------------------------------------
  History
- May.24/2018  COQ  File created.
+ May.27/2018  COQ  File created.
  -----------------------------------------------------------------------------*/
-import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
 import { FuselageComponent } from "./fuselage/fuselage.component";
-import { MotorComponent } from "./motor/motor.component";
-import { CockpitComponent } from "./cockpit/cockpit.component";
+
+const routes: Routes = [{ path: "", component: FuselageComponent }];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FuselageComponent,
-    MotorComponent,
-    CockpitComponent
-  ],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppModule {}
+export class AppRoutingModule {}
