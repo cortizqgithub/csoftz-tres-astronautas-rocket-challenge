5caaa7d8772b2d7b795fba92c6342227d5bcef6e
Carlos Adolfo Ortiz Quiros (Mac) <carlosaortiz@yahoo.com>
2018-05-27 23:32:46 -0500

    Adds these changes:
    - README.ADOC new links.
    - APP-ROUTING.MODULE.TS Adds the parameter to get the name of planet to look for.
    - APP.MODULE.TS Installs the HttpClient module.
    - PLANETS.SERVICE.TS Retrieves the REST information for given URL.
    - PLANETS.COMPONENT.CSS Makes text visible.
    - PLANETS.COMPONENT.HTML Configures the HTML template.
    - PLANETS.COMPONENT.TS Implements the whole logic to view planet.

--- app-code/rocket/src/app/planets/planets.component.ts
+++ app-code/rocket/src/app/planets/planets.component.ts
@@ -12,7 +12,9 @@
  May.25/2018  COQ  File created.
  -----------------------------------------------------------------------------*/
 import { Component, OnInit, OnDestroy } from "@angular/core";
-import { Subscription, timer, Observable } from "rxjs";
+
+import { PlanetsService } from "../planets.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-planets",
@@ -20,7 +22,87 @@ import { Subscription, timer, Observable } from "rxjs";
   styleUrls: ["./planets.component.css"]
 })
 export class PlanetsComponent implements OnInit, OnDestroy {
-  constructor() {}
+  private planetName: string;
+  private planetDegree: number;
+  private planetImg: string;
+
+  private assignPlanetInfoFor(planet: String, planetInfo: PlanetInfo) {
+    this.planetName = "";
+    this.planetDegree = 0;
+    this.planetImg = "";
+    switch (planet) {
+      case "mercurio": {
+        this.planetName = "Mercurio";
+        this.planetDegree = +planetInfo.images.mercurio.degrees;
+        this.planetImg = planetInfo.images.mercurio.img;
+        break;
+      }
+      case "venus": {
+        this.planetName = "Venus";
+        this.planetDegree = +planetInfo.images.venus.degrees;
+        this.planetImg = planetInfo.images.venus.img;
+        break;
+      }
+      case "tierra": {
+        this.planetName = "Tierra";
+        this.planetDegree = +planetInfo.images.tierra.degrees;
+        this.planetImg = planetInfo.images.tierra.img;
+        break;
+      }
+      case "marte": {
+        this.planetName = "Marte";
+        this.planetDegree = +planetInfo.images.marte.degrees;
+        this.planetImg = planetInfo.images.marte.img;
+        break;
+      }
+      case "jupiter": {
+        this.planetName = "Jupiter";
+        this.planetDegree = +planetInfo.images.jupiter.degrees;
+        this.planetImg = planetInfo.images.jupiter.img;
+        break;
+      }
+      case "saturno": {
+        this.planetName = "Saturno";
+        this.planetDegree = +planetInfo.images.saturno.degrees;
+        this.planetImg = planetInfo.images.saturno.img;
+        break;
+      }
+      case "urano": {
+        this.planetName = "Urano";
+        this.planetDegree = +planetInfo.images.urano.degrees;
+        this.planetImg = planetInfo.images.urano.img;
+        break;
+      }
+      case "neptuno": {
+        this.planetName = "Neptuno";
+        this.planetDegree = +planetInfo.images.neptuno.degrees;
+        this.planetImg = planetInfo.images.neptuno.img;
+        break;
+      }
+      case "pluton": {
+        this.planetName = "Plutón";
+        this.planetDegree = +planetInfo.images.pluton.degrees;
+        this.planetImg = planetInfo.images.pluton.img;
+        break;
+      }
+      default: {
+        this.planetName = "NO VALIDO";
+        this.planetDegree = 0;
+        this.planetImg = "";
+      }
+    }
+  }
+
+  constructor(
+    private planetService: PlanetsService,
+    private route: ActivatedRoute
+  ) {
+    let planetId: String;
+    this.route.params.subscribe(res => (planetId = res.id));
+    this.planetService.getPlanetInfo().subscribe(data => {
+      this.assignPlanetInfoFor(planetId, data);
+    });
+  }
 
   ngOnInit() {}
   ngOnDestroy(): void {}
